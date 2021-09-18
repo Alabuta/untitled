2b25173758160c2864d2e185c1d39d74eac7112b
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-09-18 22:03:27 +0300

    volk version upgrade to 1.2.190

--- main.cpp
+++ main.cpp
@@ -9,12 +9,16 @@ using namespace std::string_view_literals;
 #include <glm/glm.hpp>
 #include <fmt/format.h>
 #include <GLFW/glfw3.h>
+#include <volk/volk.h>
 #include <nlohmann/json.hpp>
 
 int main()
 {
     if (auto result = glfwInit(); result != GLFW_TRUE)
         throw std::runtime_error(fmt::format("failed to init GLFW: {0:#x}", result));
+
+    if (auto result = volkInitialize(); result != VK_SUCCESS)
+        throw std::runtime_error("failed to initialize 'volk' meta-loader"s);
 
     glm::vec2 vec{4, 8};
     nlohmann::json json;
