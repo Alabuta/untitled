c8927ab258fc4e6a1ee130b1bd6cb32b6dc2b18e
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-09-18 17:12:48 +0300

    nlohmann-json version upgrade to 3.10.2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
-#include <iostream>
+#include <string>
+using namespace std::string_literals;
+
+#include <string_view>
+using namespace std::string_view_literals;
 
 #include <glm/glm.hpp>
 #include <fmt/format.h>
+#include <nlohmann/json.hpp>
 
 int main()
 {
     glm::vec2 vec{4, 8};
-    fmt::print("Hello, World! {}:{}\n", vec.x, vec.y);
+    nlohmann::json json;
+    json["pi"s] = 3.141;
+    fmt::print("Hello, World! {}:{}\nPI is {}", vec.x, vec.y, json.dump());
 
     return 0;
 }
