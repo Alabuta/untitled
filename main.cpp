1a97df5fa5fb7916a57c211f8812d8a0872113f9
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-09-18 17:28:44 +0300

    glfw version upgrade to 3.3.4

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <exception>
+
 #include <string>
 using namespace std::string_literals;
 
@@ -6,10 +8,14 @@ using namespace std::string_view_literals;
 
 #include <glm/glm.hpp>
 #include <fmt/format.h>
+#include <GLFW/glfw3.h>
 #include <nlohmann/json.hpp>
 
 int main()
 {
+    if (auto result = glfwInit(); result != GLFW_TRUE)
+        throw std::runtime_error(fmt::format("failed to init GLFW: {0:#x}"s, result));
+
     glm::vec2 vec{4, 8};
     nlohmann::json json;
     json["pi"s] = 3.141;
