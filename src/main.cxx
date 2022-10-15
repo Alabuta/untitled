252e108fc8d9b0904a0d05f426347535847a3639
Almaz K <auqolaq@gmail.com>
2022-10-15 21:05:48 +0300

    Refactoring subdirectories usage.

--- src/main.cxx
+++ src/main.cxx
@@ -1,14 +1,17 @@
+#include <iostream>
 #include <string>
+
 using namespace std::string_literals;
 
-#include <string_view>
-using namespace std::string_view_literals;
-
 #include <glm/glm.hpp>
-#include <fmt/format.h>
-#include <GLFW/glfw3.h>
+
+#define VOLK_IMPLEMENTATION
 #include <volk/volk.h>
+
+#include <GLFW/glfw3.h>
+
 #include <nlohmann/json.hpp>
+#include <fmt/format.h>
 
 #include <boost/cstdfloat.hpp>
 
@@ -18,12 +21,13 @@ int main()
         throw std::runtime_error(fmt::format("failed to init GLFW: {0:#x}", result));
 
     if (auto result = volkInitialize(); result != VK_SUCCESS)
-        throw std::runtime_error("failed to initialize 'volk' meta-loader"s);
+        throw std::runtime_error("failed to initialize 'volk' meta-loader");
 
     boost::float32_t f = 2;
     glm::vec2 vec{4, 8};
     nlohmann::json json;
     json["pi"s] = 3.141;
+
     fmt::print("Hello, World! {}:{}\nPI is {}\nf{}", vec.x, vec.y, json.dump(), f);
 
     return 0;
